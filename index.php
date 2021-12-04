bb7ed4e6b9f63ebc68f4740d7ccddaf81461ce4d
vale <valeria.rebagliati34@gmail.com>
2021-12-04 18:03:38 +0100

    fix

--- index.php
+++ index.php
@@ -62,7 +62,7 @@
          <main>
             <div class="position-relative overflow-hidden p-md-5 m-md-3 text-center bg-warning">
                 <div class="col-md-5 p-lg-5 mx-auto my-5">
-                  <h1 class="display-4 fw-normal">PIMP MY QACK</h1>
+                  <h1 class="display-4 fw-normal">PIMP MY QUACK</h1>
                   <p class="lead fw-normal">Customize your programming ducks</p>
                   <a class="btn btn-outline-secondary" href="#">Coming soon</a>
                 </div>

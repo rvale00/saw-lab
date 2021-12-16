4619ccf26c3ef10b96cc8d5e75e44adacad5e381
F4de78 <casazucca@gmail.com>
2021-12-16 13:02:25 +0100

    cose e nuovo db

--- php/articolo.php
+++ php/articolo.php
@@ -53,21 +53,31 @@
                     echo "\t\t<div class=col-6 my-6'>\n";
                     echo "<h1>Valuta il prodotto</h1>";
                     ?>
-                    <form>
-                    <textarea>
-                        Inserisci qui il tuo commento
-                      </textarea>
-                      <script>
-                        tinymce.init({
-                          selector: 'textarea',
-                          //plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
-                          //toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
-                          toolbar_mode: 'floating',
-                          tinycomments_mode: 'embedded',
-                          tinycomments_author: 'Author name',
-                        });
-                      </script>
-                     <?php
+                    <form action="" method="get">
+                        <h4><select name='v' id='v'>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                         </select>  / 5 </h4>
+
+                        <textarea name="comment">
+                            Inserisci qui il tuo commento
+                        </textarea>
+                        <script>
+                            tinymce.init({
+                                selector: 'textarea',
+                                //plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+                                //toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
+                                toolbar_mode: 'floating',
+                                tinycomments_mode: 'embedded',
+                                tinycomments_author: 'Author name',
+                            });
+                        </script>
+                        <button type="submit">Invia</button>
+                        </form>
+                    <?php
                     echo "</div>";
                 echo "</div>";
             echo "</div>";
---
+++ php/valuta.php
@@ -0,0 +1,24 @@
+ <form action="" method="get">
+                        <h4><select name='v' id='v'>
+                            <option value='1'>1</option>
+                            <option value='2'>2</option>
+                            <option value='3'>3</option>
+                            <option value='4'>4</option>
+                            <option value='5'>5</option>
+                         </select>  / 5 </h4>
+
+                        <textarea name="comment">
+                            Inserisci qui il tuo commento
+                        </textarea>
+                        <script>
+                            tinymce.init({
+                                selector: 'textarea',
+                                //plugins: 'a11ychecker advcode casechange export formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker',
+                                //toolbar: 'a11ycheck addcomment showcomments casechange checklist code export formatpainter pageembed permanentpen table',
+                                toolbar_mode: 'floating',
+                                tinycomments_mode: 'embedded',
+                                tinycomments_author: 'Author name',
+                            });
+                        </script>
+                        <button type="submit">Invia</button>
+                        </form>

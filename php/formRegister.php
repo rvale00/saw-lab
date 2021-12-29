c16c7b77deb9659854af2fd929851e026b2c840e
F4de78 <casazucca@gmail.com>
2021-12-29 19:27:19 +0100

    register con visual. errori con bordi rossi, cose in json

--- php/formRegister.php
+++ php/formRegister.php
@@ -5,6 +5,54 @@
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/form.css">
         <script>
+            function checkInput(result){
+              if(result.fname != undefined){
+                      $('#fname').css({ "border": '#FF0000 1px solid'});
+                      $('#fnameErr').html(result.fname);
+              }else{
+                      $('#fname').removeAttr("style");
+                      $('#fnameErr').empty();              
+              }
+              if(result.lname != undefined){
+                      $('#lname').css({ "border": '#FF0000 1px solid'});
+                      $('#lnameErr').html(result.lname);
+              }else{
+                      $('#lname').removeAttr("style");
+                      $('#lnameErr').empty();              
+              }
+              if(result.email != undefined){
+                      $('#email').css({ "border": '#FF0000 1px solid'});
+                      $('#emailErr').html(result.email);
+              }else{
+                      $('#email').removeAttr("style");
+                      $('#emailErr').empty();              
+              }
+              if(result.psw != undefined){
+                      $('#psw').css({ "border": '#FF0000 1px solid'});
+                      $('#pswErr').html(result.psw);
+              }else{
+                      $('#psw').removeAttr("style");
+                      $('#pswErr').empty();              
+              }
+              if(result.cPsw != undefined){
+                      $('#cPsw').css({ "border": '#FF0000 1px solid'});
+                      $('#cPswErr').html(result.cPsw);
+              }else{
+                      $('#cPsw').removeAttr("style");
+                      $('#cPswErr').empty();              
+              }
+              /*if(result.nopsw != undefined){
+                      $('#psw').css({ "border": '#FF0000 1px solid'});
+                      $('#cPsw').css({ "border": '#FF0000 1px solid'});
+                      $('#nopsw').html(result.nopsw);
+              }else{
+                      $('#psw').removeAttr("style");
+                      $('#cPsw').removeAttr("style");
+                      $('#nopsw').empty();              
+              }*/
+            }
+
+
             function register(){
               var usermail = document.getElementsByName("email")[0].value;
               var userpsw = document.getElementsByName("psw")[0].value;
@@ -16,12 +64,14 @@
                 headers: { "Content-type": "application/x-www-form-urlencoded" },
                 body: "name="+ userfname+ "&surname="+ userlname + "&email=" + usermail + "&psw=" + userpsw + "&cPsw=" + usercpsw,
                 }).then(function (response) { 
-                    console.log(response.statusText);
-                    return response.text();
+                    return response.json();
                 }).then(function (result) {
-                    alert(result);
-                    document.getElementById('regForm').innerHTML = "<h1>Account registrato con successo</h1> \
-                                                                     <a class='btn btn-primary' href='formLogin.php'> Accedi </a>";
+                    checkInput(result);
+                    //alert(JSON.stringify(result));
+                    
+
+                    //document.getElementById('regForm').innerHTML = "<h1>Account registrato con successo</h1> \
+                      //                                               <a class='btn btn-primary' href='formLogin.php'> Accedi </a>";
                     
                 });
             }
@@ -39,28 +89,56 @@
         <h1 class="h3 mb-3 fw-normal">Please register</h1>
 
         <div class="form-floating">
-          <input type="text" class="form-control" id="fname" name="name" placeholder="Rino">
-          <label for="floatingInput">Name</label>
+          <input type="text" class="form-control bg-error" id="fname" name="name" placeholder="Rino">
+          <p id="fnameErr" ></p>
+          <label for="floatingInput" >Nome*</label>
         </div>
 
         <div class="form-floating">
           <input type="text" class="form-control" id="lname" name="surname" placeholder="Pape">
-          <label for="floatingInput">Surname</label>
+          <p id="lnameErr" ></p>
+          <label for="floatingInput">Cognome*</label>
         </div>
 
         <div class="form-floating">
           <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com">
-          <label for="floatingInput">Email address</label>
+          <p id="emailErr" ></p>
+          <label for="floatingInput">Email*</label>
         </div>
         <div class="form-floating">
           <input type="password" class="form-control" id="psw" name="psw" placeholder="Password">
-          <label for="floatingPassword">Password</label>
+          <p id="pswErr" ></p>
+          <label for="floatingPassword">Password*</label>
         </div>
 
         <div class="form-floating">
           <input type="password" class="form-control" id="cPsw" name="cPsw" placeholder="Re Password">
-          <label for="floatingPassword">Confirm password</label>
+          <p id="cPswErr" ></p>
+          <label for="floatingPassword">Conferma password*</label>
         </div>
+        
+        <p id="nopsw" ></p>
+
+        <div class="form-floating">
+          <input type="text" class="form-control" id="reg" name="reg" placeholder="Regione">
+          <label for="floatingInput">Regione</label>
+        </div>
+
+        <div class="form-floating">
+          <input type="text" class="form-control" id="citta" name="citta" placeholder="Citta'">
+          <label for="floatingInput">Citta'</label>
+        </div>
+
+        <div class="form-floating">
+          <input type="text" class="form-control" id="ind" name="ind" placeholder="Indirizzo">
+          <label for="floatingInput">Indirizzo</label>
+        </div>
+
+        <div class="form-floating">
+          <input type="text" class="form-control" id="cap" name="cap" placeholder="Cap">
+          <label for="floatingInput">Cap</label>
+        </div>
+
 
         <a type="button" onclick="register()" class="w-100 btn btn-lg btn-primary">Sign in</a>
         <p class="mt-5 mb-3 text-muted">&copy; 2017–2021</p>
--- php/register.php
+++ php/register.php
@@ -5,40 +5,48 @@
     include("../db/connect.php");
 
     $name = $surname = $email = $password = $cpassword = " ";
+    $err = array();
     if($_SERVER["REQUEST_METHOD"] == "POST"){
         if(empty($_POST["name"])){
-            echo "<p> e' necessario inserire il nome</p>";
-            exit;
+            $err += array("fname" => " e' necessario inserire il nome");
         }else{
             $name = $_POST["name"];
         }
+
         if(empty($_POST["surname"])){
-            echo "<p> e' necessario inserire il cognome</p>";
-            exit;
+            $err += array("lname" =>" e' necessario inserire il cognome");
         }else{
             $surname = $_POST["surname"];
         }
+
         if(empty($_POST["email"])){
-            echo "<p> e' necessario inserire l'email</p>";
-            exit;
+            $err += array("email" =>" e' necessario inserire l'email");
         }else{
             $email = $_POST["email"];
         }
+
         if(empty($_POST["psw"])){
-            echo "<p> e' necessario inserire una password</p>";
-            exit;
+            $err += array("psw" =>" e' necessario inserire una password");
         }else{
             $password = $_POST["psw"];
         }
+
         if(empty($_POST["cPsw"])){
-            echo "<p> e' necessario confermare la password</p>";
-            exit;
+            $err += array("cPsw" =>" e' necessario confermare la password");
         }else{
             $cpassword = $_POST["cPsw"];
         }
+
         if($password != $cpassword){
-            echo "<p> le password non corrispondono.</p>";
-            exit;
+            $err += array("nopsw" =>" le password non corrispondono.");
+        }
+        if (!(filter_var($email, FILTER_VALIDATE_EMAIL))) {
+            $err += array("email"=>"mail non valida");
+        }
+        //controlla se ci sono campi vuoti
+        if (!empty($err)){
+            echo json_encode($err);
+            exit();
         }
 
         $conn = connectDB();
@@ -50,26 +58,28 @@
         $surname=trim($surname);
         $newsname = filter_var($surname, FILTER_SANITIZE_STRING);
         mysqli_real_escape_string($conn, $email);
-        $email = filter_var($email, FILTER_SANITIZE_EMAIL);
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL) === true) {
-                    echo("$email is invalid");
-                }
         $password = trim($password);
+        
         $hashedpsw = password_hash($password,PASSWORD_DEFAULT);
 
         $stmt = mysqli_prepare($conn,"INSERT INTO utenti (email,psw,_name,_surname) VALUES (?,?,?,?)");
         mysqli_stmt_bind_param($stmt, 'ssss', $email,$hashedpsw,$name,$surname);
 
-        mysqli_stmt_execute($stmt); 
+        if(!mysqli_stmt_execute($stmt)){
+            echo json_encode(array("pkErr"=>"Errore: mail gia' usata"));
+
+            mysqli_close($conn);
+            exit();
+        }
+
         if(mysqli_affected_rows($conn) === 0){
-            echo"query error";
+            echo json_encode(array("noAffRow"=>"Errore durante la registrazione"));
 
             mysqli_close($conn);
             exit();
             
         }
-        echo "Registered.";
-        echo " <a href='formLogin.php'> Accedi </a>";
+        echo json_encode(array("ok"=>"Registrato con successo!"));
         mysqli_close($conn);
 
 

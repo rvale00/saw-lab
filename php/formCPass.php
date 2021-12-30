acf5a980d04b4f0f71b92c5a6d333a7ecbc50590
F4de78 <casazucca@gmail.com>
2021-12-30 18:08:59 +0100

    fix

--- php/formCPass.php
+++ php/formCPass.php
@@ -3,81 +3,26 @@
     <head lang="it">
         <title>Cambio password</title>
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-    <style>
-        html,
-    body {
-      height: 100%;
-    }
-
-    body {
-      display: flex;
-      align-items: center;
-      padding-top: 40px;
-      padding-bottom: 40px;
-      background-color: #f5f5f5;
-    }
-
-    .form-signin {
-      width: 100%;
-      max-width: 330px;
-      padding: 15px;
-      margin: auto;
-    }
-
-    .form-signin .checkbox {
-      font-weight: 400;
-    }
-
-    .form-signin .form-floating:focus-within {
-      z-index: 2;
-    }
-
-    .form-signin input[type="email"] {
-      margin-bottom: -1px;
-      border-bottom-right-radius: 0;
-      border-bottom-left-radius: 0;
-    }
-
-    .form-signin input[type="password"] {
-      margin-bottom: 10px;
-      border-top-left-radius: 0;
-      border-top-right-radius: 0;
-    }
-
-.bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-        </style>
-
-
+        <link rel="stylesheet" href="../css/form.css">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
 
     <script>
             function checkData(){
               if(result.psw != undefined){
-                $('#psw').css({ "border": '#FF0000 1px solid'});
-                $('#pswErr').html(result.psw);
+                $('#oldpsw').css({ "border": '#FF0000 1px solid'});
+                $('#oldpswErr').html(result.psw);
                
               }else{
-                $('#psw').removeAttr("style");
-                $('#pswErr').empty();              
+                $('#oldpsw').removeAttr("style");
+                $('#oldpswErr').empty();              
               }
               if(result.cPsw != undefined){
-                $('#cPsw').css({ "border": '#FF0000 1px solid'});
-                $('#cPswErr').html(result.cPsw);
+                $('#newpsw').css({ "border": '#FF0000 1px solid'});
+                $('newpswErr').html(result.cPsw);
                
               }else{
-                $('#cPsw').removeAttr("style");
-                $('#cPswErr').empty();
+                $('#newpsw').removeAttr("style");
+                $('#newpswErr').empty();
 
                 //se entrambi i campi hanno del contenuto confronto che siano uguali
                 if(result.nopsw != undefined){
